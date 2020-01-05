60e436f4dbb6b230ee1d279075173f00f369fcab
Alex Kreskiyan <a.kreskiyan@gmail.com>
2020-01-05 03:29:49 +0300

    Fix MessagePackSerializer

--- base/Serialization/src/Annium.Serialization.MessagePack/MessagePackSerializer.cs
+++ base/Serialization/src/Annium.Serialization.MessagePack/MessagePackSerializer.cs
@@ -1,3 +1,4 @@
+using System;
 using Annium.Serialization.Abstractions;
 using MessagePack;
 
@@ -8,7 +9,7 @@ namespace Annium.Serialization.MessagePack
         private static readonly MessagePackSerializerOptions Opts =
             MessagePackSerializerOptions.Standard.WithCompression(MessagePackCompression.Lz4BlockArray);
 
-        public static ISerializer<byte[]> Instance { get; } = Serializer.Create(
+        public static ISerializer<ReadOnlyMemory<byte>> Instance { get; } = Serializer.Create<ReadOnlyMemory<byte>>(
             value => global::MessagePack.MessagePackSerializer.Serialize(value, Opts),
             (type, value) => global::MessagePack.MessagePackSerializer.Deserialize(type, value, Opts)
         );
